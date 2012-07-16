3cf512e3f61dae2705c839de031287c2b5d6ef2c
Ricky Ng-Adam <rngadam@lophilo.com>
2012-07-16 22:21:20 +0800

    with partition size verification

--- mkcard.py
+++ mkcard.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 # sudo apt-get install python-parted
+# sudo apt-get install python-git
+# sudo apt-get install usbmount
 
-# doc:
+# doc for parted:
 # https://fedorahosted.org/pyparted/
-
 # code examples:
-
 # http://amebasystems.googlecode.com/svn-history/r233/research/a-i/stage-2/anaconda
 # http://linux1.fnal.gov/linux/fermi/obsolete/lts309/x86_64/sites/Fermi/RHupdates/fsset.py
 # http://magicinstaller2.googlecode.com/svn-history/r34/trunk/src/magic.installer/operations/parted.py
+
+# doc for git
+# http://packages.python.org/GitPython/0.1/intro.html#getting-started
 
 import _ped
 import os
@@ -17,17 +20,14 @@ import code
 import traceback 
 import parted
 import shutil
+import git
+
 from collections import OrderedDict
 from subprocess import check_call
 
 class mkcardException(Exception):
 	pass
 
-LINUX_SWAP = _ped.file_system_type_get("linux-swap")
-LINUX_NATIVE = _ped.file_system_type_get("ext4")
-WINDOWS_FAT16 = _ped.file_system_type_get("fat16")
-
-DISK_TYPE = _ped.disk_type_get("msdos")
 
 kcmd_default = OrderedDict({
 	'mem': '128M',
@@ -47,64 +47,83 @@ kcmd_nfs = OrderedDict({
 	'rootfstype': 'nfs',
 })
 
+#TODO: calculate start/end or use constraints...
+# configuration below assumes 8GB sd card...
+partitions = [
+	{
+	'start': 2048,
+	'end': 1044224,
+	'type': _ped.file_system_type_get("fat16")
+	},
+	{
+	'start': 1044225,
+	'end': 14362109,
+	'type': _ped.file_system_type_get("ext4")		
+	},
+	{
+	'start': 14362110,
+	'end': 15550919,
+	'type': _ped.file_system_type_get("linux-swap")	
+	}
+]
+
+
 device_path = "/dev/sdc"
 firmware_path = "/home/rngadam/lophilo/upstream/firmware-binaries"
 os_path = "/home/rngadam/lophilo.nfs"
-rsync_command = "rsync -avz --delete-delay --delete-excluded --exclude-from excluded-files"
+rsync_command = "rsync -avz --delete-delay --exclude-from excluded-files"
+rsync_command_delete_excluded = "%s --delete-excluded" % rsync_command
+target_rev = 'tabbyrev1'
 
 if os.getenv("USER") != "root":
 	raise mkcardException("Must be run as root")
 
-def create_partitions(device_path):
-
+def verify_partitions(device_path, partitions):
 	device = parted.getDevice(device_path)
 	disk = parted.Disk(device)
-	for part in disk.partitions:
-		print part
+	for part_id in xrange(0, len(partitions)):
+		current_partition = disk.partitions[part_id]
+		target_partition = partitions[part_id]
+		
+		current_partition_start = current_partition.getPedPartition().geom.start
+		target_partition_start = target_partition['start']
+		if current_partition_start != target_partition_start:
+			print "start is not the same. current: %d target: %d" % (current_partition_start, target_partition_start)
+			return False
+
+		current_partition_end = current_partition.getPedPartition().geom.end
+		target_partition_end = target_partition['end']
+		if current_partition_end != target_partition_end:
+			print "end is not the same. current: %d target: %d" % (current_partition_end, target_partition_end)
+			return False
+
+
 		# check if the partition are in use... if yes, abort
-		if part.busy:
-			raise mkcardException("Partition is busy" + part)
+		if current_partition.busy:
+			raise mkcardException("Partition is busy %s, please umount first" % current_partition)
+	return True
+
+
+def create_partitions(device_path, partitions):
+	device = parted.getDevice(device_path)
+	disk = parted.Disk(device)
 
 	# prompt user before wiping out disk...
 	disk.deleteAllPartitions()
 	disk.commitToDevice()
 	disk.commitToOS()
 
-	#TODO: calculate start/end or use constraints...
-	# configuration below assumes 8GB sd card...
-	start = 2048
-	fat_length = (512*1024*1024) / device.sectorSize
-	swap_length = (512*1024*1024) / device.sectorSize
-	ext4_length = device.length - (fat_length+swap_length+start)
-
-	fat_start = 2048
-	fat_end = 1050623
-
-	ext4_start = 1050624
-	ext4_end = 14362623
-
-	swap_start = 14362624
-	swap_end = 15556607
-
 	# create volatile store for new partition information
 	# create fat, ext4 and swap partition
- 	disk = parted.freshDisk(device, DISK_TYPE)
+ 	disk = parted.freshDisk(device, _ped.disk_type_get("msdos"))
+
  	constraint = parted.Constraint(device=device)
 
-	geometry = parted.Geometry(device=device, start=fat_start, end=fat_end)
-	partition = parted.Partition(disk=disk, type=0, geometry=geometry)
-	disk.addPartition(partition,constraint=constraint)
-	partition.getPedPartition().set_system(WINDOWS_FAT16)
-	
-	geometry = parted.Geometry(device=device, start=ext4_start, end=ext4_end)
-	partition = parted.Partition(disk=disk, type=parted.PARTITION_NORMAL, geometry=geometry)
-	disk.addPartition(partition,constraint=constraint)
-	partition.getPedPartition().set_system(LINUX_NATIVE)	
-
-	geometry = parted.Geometry(device=device, start=swap_start, end=swap_end)
-	partition = parted.Partition(disk=disk, type=parted.PARTITION_NORMAL, geometry=geometry)
-	disk.addPartition(partition,constraint=constraint)
-	partition.getPedPartition().set_system(LINUX_SWAP)		
+ 	for partition in partitions:
+		geometry = parted.Geometry(device=device, start=partition.start, end=partition.end)
+		partition = parted.Partition(disk=disk, type=parted.PARTITION_NORMAL, geometry=geometry)
+		disk.addPartition(partition,constraint=constraint)
+		partition.getPedPartition().set_system(partition.type)
 
 	disk.commit()
 
@@ -128,7 +147,7 @@ def sync_os(device_path, firmware_path, os_path):
 
 	# now mounted as /media/BOOT and /media/os
 	simple_call("%s %s/ %s/" % (rsync_command, firmware_path, target_boot_dir))
-	simple_call("%s %s/ %s/" % (rsync_command, os_path, target_os_dir))
+	simple_call("%s %s/ %s/" % (rsync_command_delete_excluded, os_path, target_os_dir))
 
 	# slightly different configuration between
 	# NFS boot and MicroSD card boot
@@ -157,10 +176,26 @@ def create_cmd(kcmd, overrides=None):
 	params.extend([k for k in kcmd if not kcmd[k]])
 	return ' '.join(params)
 
-try:
-	create_partitions(device_path)
+def verify_repos(target_rev, repo_paths):
+	for repo_path in repo_paths:
+		repo = git.Repo(repo_path)
+		if repo.active_branch != target_rev:
+			raise mkcardException(
+				'Expecting repo %s branch to match hardware rev %s (currently: %s)' % (
+					repo, target_rev, repo.active_branch))
+		else:
+			print 'OK: repo %s active_branch is %s' % (repo, target_rev)
 
-	format_disks(device_path)
+try:
+	
+	if verify_partitions(device_path, partitions):
+		print "partitions are OK"
+	else:
+		print "partitions don't match target, recreating"	
+		create_partitions(device_path, partitions)
+		format_disks(device_path)
+
+	verify_repos(target_rev, [firmware_path, os_path])
 
 	sync_os(device_path, firmware_path, os_path)
 
